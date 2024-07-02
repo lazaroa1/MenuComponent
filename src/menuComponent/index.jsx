88ef9006d9728db8eb51e5a4afde0df98a4bf215
lazaroa1 <lazaroalves36@gmail.com>
2024-07-02 11:50:08 -0300

    feature: quando for fechar o pai fecha os filhos que estiverem aberto

--- src/menuComponent/index.jsx
+++ src/menuComponent/index.jsx
@@ -1,16 +1,27 @@
 import { useEffect, useMemo, useState } from "react";
 import "./index.css";
 
-const MenuItem = ({ item, onToggle }) => {
+const MenuItem = ({ item, onToggle, hasSubMenuWithSubMenu }) => {
+  const hasSubMenu = item.subMenu && !!item.subMenu.length;
+  const isSideMenu = hasSubMenu && hasSubMenuWithSubMenu(item.subMenu);
+
   return (
-    <div>
+    <div className="menu-item-container">
       <button className="menu-item" onClick={() => onToggle(item)}>
         {item.description}
+        {hasSubMenu && <span className="arrow">{item.isOpen ? "▲" : "▼"}</span>}
       </button>
       {item.isOpen && item.subMenu && (
-        <div className="submenu">
+        <div
+          className={`submenu ${isSideMenu ? "submenu-side" : "submenu-below"}`}
+        >
           {item.subMenu.map((subItem) => (
-            <MenuItem key={subItem.id} item={subItem} onToggle={onToggle} />
+            <MenuItem
+              key={subItem.id}
+              item={subItem}
+              onToggle={onToggle}
+              hasSubMenuWithSubMenu={hasSubMenuWithSubMenu}
+            />
           ))}
         </div>
       )}
@@ -22,38 +33,43 @@ export default function MenuComponent({ menuItem = [] }) {
   const [_menuItem, setMenuItem] = useState([]);
 
   function addIsOpen(data) {
-    const addIsOpenRecursively = (items) => {
-      const itemsWithIsOpen = items.map((item) => {
-        if (item.subMenu) {
-          return {
-            ...item,
-            isOpen: false,
-            subMenu: addIsOpenRecursively(item.subMenu),
-          };
-        }
-        return { ...item };
-      });
-      return itemsWithIsOpen;
-    };
-
-    return addIsOpenRecursively(data);
+    const itemsWithIsOpen = data.map((item) => {
+      if (item.subMenu) {
+        return {
+          ...item,
+          isOpen: false,
+          subMenu: addIsOpen(item.subMenu),
+        };
+      }
+      return { ...item };
+    });
+    return itemsWithIsOpen;
   }
 
   function updateIsOpen(data, id) {
-    const updateIsOpenRecursively = (items) => {
-      const updateIsData = items.map((item) => {
-        if (item.id === id) {
-          return { ...item, isOpen: !item.isOpen };
-        }
-        if (!!item.subMenu) {
-          return { ...item, subMenu: updateIsOpenRecursively(item.subMenu) };
-        }
-        return item;
-      });
-      return updateIsData;
-    };
-
-    setMenuItem(updateIsOpenRecursively(data));
+    const updateIsData = data.map((item) => {
+      if (item.id === id) {
+        return {
+          ...item,
+          isOpen: !item.isOpen,
+          subMenu: !item.isOpen ? closeAllSubMenus(item.subMenu) : item.subMenu,
+        };
+      }
+      if (item.isOpen && item?.subMenu) {
+        return {
+          ...item,
+          // isOpen: false,
+          subMenu: updateIsOpen(item.subMenu, id),
+        };
+      }
+      /*
+      if (!!item.subMenu) {
+        return { ...item, subMenu: updateIsOpen(item.subMenu, id) };
+      }
+      */
+      return item;
+    });
+    return updateIsData;
   }
 
   const initializedMenuItems = useMemo(() => addIsOpen(menuItem), [menuItem]);
@@ -67,19 +83,42 @@ export default function MenuComponent({ menuItem = [] }) {
 
   function handleSubMenu(item) {
     if (!!item?.subMenu) {
-      updateIsOpen(_menuItem, item.id);
+      toggleIsOpen(item.id);
     } else {
       alert("Item sem submenu");
     }
   }
 
+  function hasSubMenuWithSubMenu(subMenu) {
+    return subMenu.some((item) => item.subMenu && item.subMenu.length > 0);
+  }
+
+  function toggleIsOpen(id) {
+    setMenuItem((prevState) => updateIsOpen(prevState, id));
+  }
+
+  function closeAllSubMenus(items) {
+    return items?.map((item) => ({
+      ...item,
+      isOpen: false,
+      subMenu: closeAllSubMenus(item.subMenu),
+    }));
+  }
+
+  function hasSubMenuWithSubMenu(subMenu) {
+    return subMenu.some((item) => item.subMenu && item.subMenu.length > 0);
+  }
+
   return (
-    <div className="container">
-      <div className="menu-dropdown">
-        {_menuItem?.map((item) => (
-          <MenuItem key={item.id} item={item} onToggle={handleSubMenu} />
-        ))}
-      </div>
+    <div className="menu-container">
+      {_menuItem?.map((item) => (
+        <MenuItem
+          key={item.id}
+          item={item}
+          onToggle={handleSubMenu}
+          hasSubMenuWithSubMenu={hasSubMenuWithSubMenu}
+        />
+      ))}
     </div>
   );
 }
